9b1924505e11003c66480044f199049525f101a7
Jonas <jonas.mokawane98@gmail.com>
2024-08-15 08:54:49 +0200

    Updated Header.vue: changed router.back to router.push in navigation button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,7 +24,7 @@ const router = useRouter();
     <nav class="bg-gray-500 border-gray-200 ">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <button 
-             @click="() => router.back(`/`)"
+             @click="() => router.push(`/`)"
             class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="/online-shop.png" class="h-8" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
